0ecec9ba42f570f494def32ff8794485490011b8
Eric Jensen <magnesium38@gmail.com>
2017-03-02 00:07:06 -0600

    Complete assignment. Read_char was weird though. Using read_int temporarily.

--- Homework1.asm
+++ Homework1.asm
@@ -1,5 +1,5 @@
 %include "asm_io.inc"
-segment .data 
+segment .data
 PromptFileUnit		db	"Enter the unit for file size:", 0
 PromptThroughUnit	db	"Enter the unit for throughput:", 0
 PromptFileSize		db	"Enter the file size:", 0
@@ -9,31 +9,48 @@ Seconds			db	" seconds.", 0
 
 InvalidInput		db	"Invalid input.", 0
 
-segment .bss 
+segment .bss
 fileMultiplier		resd 1
 throughMultiplier	resd 1
-
+fileUnit		resd 1
+throughUnit		resd 1
+fileSize		resd 1
+throughSize		resd 1
 
 segment .text
-        global  asm_main
+	global	asm_main
 asm_main:
-        enter   0,0               ; setup routine
-        pusha
+	enter	0,0               ; setup routine
+	pusha
 	;***************CODE STARTS HERE***************************
 
 	mov	dword [fileMultiplier], 8
 	mov	dword [throughMultiplier], 1
 
+	; Read all input.
 	mov	eax, PromptFileUnit
 	call	print_string
-	call	read_char
+	call	read_int
+	mov	[fileUnit], eax
 
-	;call	LoadBKMValues
-	mov	ebx, 0x00000042
-	mov	ecx, 0x0000004B
-	mov	edx, 0x0000004D
+	mov	eax, PromptThroughUnit
+	call	print_string
+	call	read_int
+	mov	[throughUnit], eax
 
-	dump_regs 3
+	mov	eax, PromptFileSize
+	call	print_string
+	call	read_int
+	mov	[fileSize], eax
+
+	mov	eax, PromptThroughSize
+	call	print_string
+	call	read_int
+	mov	[throughSize], eax
+
+	; Calculate multipliers
+	call	LoadBKMValues
+	mov	eax, [fileUnit]
 
 	cmp	eax, ebx
 	jz	throughput
@@ -44,32 +61,17 @@ asm_main:
 	cmp	eax, edx
 	jz	megaFile
 
+	dump_regs 9
+
 	jmp	invalid
 kiloFile:
 	shl	dword [fileMultiplier], 10
 
-	;mov	eax, [fileMultiplier]
-	;mul	0x00000400 ; 1024
-	;mov	[fileMultiplier], eax
-
 	jmp	throughput
 megaFile:
 	shl	dword [fileMultiplier], 20
-
-	;mov	eax, [fileMultiplier]
-	;mul	0x00100000 ; 1024 * 1024
-	;mov	[fileMultiplier], eax
 throughput:
-	dump_regs 0
-
-	mov	eax, PromptThroughUnit
-	call	print_string
-
-	dump_regs 2
-	call	read_int
-
-	dump_regs 1
-
+	mov	eax, [throughUnit]
 	cmp	eax, ebx
 	jz	calculate
 
@@ -79,42 +81,42 @@ throughput:
 	cmp	eax, edx
 	jz	megaThrough
 
+	dump_regs 10
+
 	jmp	invalid
 kiloThrough:
 	shl	dword [throughMultiplier], 10
 
-	jmp calculate
+	jmp	calculate
 megaThrough:
 	shl	dword [throughMultiplier], 20
 calculate:
-	mov	eax, PromptFileSize
-	call	print_string
-
+	; Do calculations.
 	mov	eax, 0
 	mov	edx, 0
 
-	call	read_int
+	mov	eax, [fileSize]
 	mul	dword [fileMultiplier]
 	push	eax
 	push	edx
 
-	mov	eax, PromptThroughSize
-	call	print_string
-
-	mov	eax, Output
-	call	print_string
-
 	mov	edx, 0
-	call	read_int
+	mov eax, [throughSize]
 	mul	dword [throughMultiplier]
 
 	mov	ecx, eax
 	mov	ebx, edx
 
-	pop	eax
 	pop	edx
+	pop	eax
 
 	div	ecx
+	push	eax
+
+	mov	eax, Output
+	call	print_string
+
+	pop	eax
 	call	print_int
 
 	mov	eax, Seconds
@@ -123,15 +125,14 @@ calculate:
 invalid:
 	mov	eax, InvalidInput
 	call	print_string
-
-exit:	
+exit:
 	call	print_nl
-		
+
 	;***************CODE ENDS HERE*****************************
-        popa
-        mov     eax, 0            ; return back to C
-        leave                     
-        ret
+	popa
+	mov     eax, 0            ; return back to C
+	leave
+	ret
 
 
 ; subprogram LoadBKMValues
@@ -140,7 +141,7 @@ exit:
 ; Notes:
 ;	Overwrites ebx, ecx, and edx
 LoadBKMValues:
-	enter 0, 0
+	enter	0, 0
 
 	mov	ebx, 0x00000042 ; B
 	mov	ecx, 0x0000004B ; K
